9edccc48b08a1f37bd7f34d18bc9e4232c901549
ameni ben marzoug <ameni@localhost.localdomain>
2020-10-28 18:06:15 +0100

    theme

--- src/main/java/com/eniso/tama/controller/ThemeController.java
+++ src/main/java/com/eniso/tama/controller/ThemeController.java
@@ -3,8 +3,11 @@ package com.eniso.tama.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eniso.tama.entity.ClassRoom;
 import com.eniso.tama.entity.Equipments;
+import com.eniso.tama.entity.Program;
 import com.eniso.tama.entity.Theme;
-
-
+import com.eniso.tama.payload.MessageResponse;
+import com.eniso.tama.repository.ClassRoomRepository;
+import com.eniso.tama.repository.ProgramRepository;
 import com.eniso.tama.service.ThemeService;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -27,6 +33,9 @@ import com.eniso.tama.service.ThemeService;
 @ComponentScan(basePackageClasses = ThemeService.class )
 @RequestMapping(value="/api")
 public class ThemeController {
+	
+	@Autowired
+	ProgramRepository programRepository;
 	private ThemeService themeService;
 	@Autowired
 	public ThemeController(ThemeService themeService) {
@@ -70,6 +79,27 @@ public class ThemeController {
 		
 		themeService.save(theTheme);
 		return theTheme;
+	}
+	@PostMapping("/themeProgram")
+	public ResponseEntity<?> addThemeProgram(@Valid @RequestBody Theme theme,@RequestParam("id") long id ) {
+	
+		
+		Program program = new Program();
+		for (Program p : programRepository.findAll()) {
+			if (id == p.getId()) {
+				program = p;
+			}
+		}
+		
+		Theme t = new Theme();
+		t.setThemeName(theme.getThemeName());
+		t.setNbDaysTheme(theme.getNbDaysTheme());;
+		t.setProgram(program);;
+		
+		themeService.save(t);
+		return ResponseEntity.ok(new MessageResponse("Theme added successfully!"));
+
+		
 	}
 	@PutMapping("/theme")
 	public Theme updateTheme (@RequestBody Theme theTheme) {
