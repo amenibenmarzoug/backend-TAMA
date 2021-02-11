fc93dd9bce9630642bb0e60decf2919c2c99f099
Nadia Fekih <nadiafekih.97@gmail.com>
2021-02-11 16:32:06 +0100

    Module-ThemeDetail

--- src/main/java/com/eniso/tama/controller/ThemeDetailController.java
+++ src/main/java/com/eniso/tama/controller/ThemeDetailController.java
@@ -1,5 +1,6 @@
 package com.eniso.tama.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eniso.tama.entity.Module;
 import com.eniso.tama.entity.ThemeDetail;
 import com.eniso.tama.service.ThemeDetailService;
 
@@ -31,6 +35,20 @@ public class ThemeDetailController {
 	@GetMapping("/themeDetail")
 	public List<ThemeDetail> findAll() {
 		return themeDetailService.findAll();
+	}
+	
+	@GetMapping("/module/themesDetails")
+	public List<ThemeDetail> getModuleThemeDetails(@RequestParam("id") long id) {
+		List<ThemeDetail> themeDetailsPerModule = new ArrayList<ThemeDetail>();
+		for (ThemeDetail theTD : themeDetailService.findAll()) {
+		if(theTD.getModule()!=null) {
+			if (id == theTD.getModule().getId()) {
+
+				themeDetailsPerModule.add(theTD);			
+			}
+		}
+		}
+		return themeDetailsPerModule;
 	}
 	
 	@GetMapping("themeDetail/{themeDetailId}")
